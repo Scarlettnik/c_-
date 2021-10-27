89d00a0d7409913d19c6bbbd56f9820ecc5472a8
Scarlettnik <87085957+Scarlettnik@users.noreply.github.com>
2021-10-27 11:21:52 +0300

    Add files via upload

---
+++ с_1/1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void main() {
+    double pi = 3.14159265;
+
+    printf("%.2f\n", pi);
+    printf("%.4f\n", pi);
+    printf("%.8f\n", pi);
+}
---
+++ с_1/2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void main() {
+    double a, b, c;
+    scanf("%lf %lf %lf", &a, &b, &c);
+
+    double value = a * b * c;
+    printf("%f\n", value);
+}
